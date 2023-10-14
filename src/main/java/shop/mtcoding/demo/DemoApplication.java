db141aab0b7bd901a661c43bc5f154e1e6fd9a96
jyy0719 <72611029+jyy0719@users.noreply.github.com>
2023-10-15 08:56:51 +0900

    Update DemoApplication.java

--- src/main/java/shop/mtcoding/demo/DemoApplication.java
+++ src/main/java/shop/mtcoding/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	//git pull test 주석 추가
+	//git fetch test 주석 추가
+	// git pull test 주석 추가 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
