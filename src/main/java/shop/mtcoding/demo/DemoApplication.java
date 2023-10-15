e2245cc76838ebb67a84b96f321651848536c67e
jyy0719 <72611029+jyy0719@users.noreply.github.com>
2023-10-15 09:01:33 +0900

    Update DemoApplication.java

--- src/main/java/shop/mtcoding/demo/DemoApplication.java
+++ src/main/java/shop/mtcoding/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	//git fetch test 주석 추가
 	// git pull test 주석 추가 
+	// git 로컬과 소스파일을 동시에 바꾼다면 ?
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
