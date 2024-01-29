d01d83f31494bcdd5a3175458f5cbac199bf7ac5
jyy0719 <jyy0719@naver.com>
2024-01-29 22:57:46 +0900

    🚀주석추가

--- src/main/java/shop/mtcoding/demo/DemoApplication.java
+++ src/main/java/shop/mtcoding/demo/DemoApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	// developer2 workspace branch test
+	// 주석추가
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Integer i = new Integer(4);
+		Integer i1 = new Integer(5);
+		Integer integer = new Integer(7);
 	}
 
 }
